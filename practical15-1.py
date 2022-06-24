bbcd21e21d1667b0696f0e2afb1bed9987cf7c3a
ChigasovaN <107987365+ChigasovaN@users.noreply.github.com>
2022-06-24 19:59:24 +0300

    Добавлена практическая №15

---
+++ practical15-1.py
@@ -0,0 +1,19 @@
+'''
+Практическая №15
+Вариант №32
+1. Если в матрице имеются положительные элементы, то вывести TRUE, иначе FALSE.
+'''
+matrix = [[-1, 3, -6],
+          [-8, 19, 41],
+          [23, -1, 6]]
+
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        if matrix[i][j] > 0:
+            print('TRUE')
+        else:
+            print('FALSE')
+
+        break
+    break
